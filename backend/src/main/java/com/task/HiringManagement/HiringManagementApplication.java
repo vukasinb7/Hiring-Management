bb3d612f8ea331ab515d084d4fed890fbb102f25
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2023-04-21 17:20:02 +0200

    Init Project
    Initialized project, generated gitignore for frontend and backend.

---
+++ backend/src/main/java/com/task/HiringManagement/HiringManagementApplication.java
@@ -0,0 +1,13 @@
+package com.task.HiringManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringManagementApplication.class, args);
+	}
+
+}

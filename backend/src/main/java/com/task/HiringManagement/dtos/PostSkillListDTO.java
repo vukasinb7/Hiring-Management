76cdf6f81d458dcfd688748d8fe5980dd516dec9
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2023-04-25 01:34:10 +0200

    Added Controller Tests

--- backend/src/main/java/com/task/HiringManagement/dtos/PostSkillListDTO.java
+++ backend/src/main/java/com/task/HiringManagement/dtos/PostSkillListDTO.java
@@ -1,15 +1,13 @@
 package com.task.HiringManagement.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class PostSkillListDTO {
     private List<Long> skillIds;
 }
--- backend/src/test/java/com/task/HiringManagement/controllers/SkillControllerTest.java
+++ backend/src/test/java/com/task/HiringManagement/controllers/SkillControllerTest.java
@@ -56,18 +56,40 @@ public class SkillControllerTest {
         PostSkillDTO skillDTO= new PostSkillDTO("Java");
         Skill skillExp= new Skill("Java",1L);
         Mockito.when(skillService.insert(skillDTO)).thenReturn(skillExp);
-
-        System.out.println(objectMapper.writeValueAsString(skillDTO));
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/api/skill")
                         .content(objectMapper.writeValueAsString(skillDTO))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.is(1)))
+                .andExpect(jsonPath("$.name", Matchers.is("Java")));
     }
 
     @Test
-    @DisplayName("Get Skill - Case: Positive")
+    @DisplayName("Update Skill - Case: Positive")
+    public void updateSkillPositive() throws Exception {
+        PostSkillDTO skillDTO= new PostSkillDTO("Java");
+        Skill skillExp= new Skill("Java",1L);
+        Mockito.when(skillService.update(skillDTO,1L)).thenReturn(skillExp);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/skill/1")
+                        .content(objectMapper.writeValueAsString(skillDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.is(1)))
+                .andExpect(jsonPath("$.name", Matchers.is("Java")));
+    }
+
+    @Test
+    @DisplayName("Update Skill - Case: Not a number")
+    public void UpdateSkill400Format() throws Exception {
+        mockMvc.perform(put("/api/skill/aa")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Delete Skill - Case: Positive")
     public void deleteSkillPositive() throws Exception {
 
         doNothing().when(skillService).delete(1L);
